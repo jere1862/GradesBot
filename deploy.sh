ec3e21aff18c8720efd68b261b6bee3ada54ef3d
Jeremie Roy <j-roy95@hotmail.com>
2017-12-13 18:31:36 -0500

    Small fixes to deploy file

--- deploy.sh
+++ deploy.sh
@@ -8,13 +8,11 @@ usermod -a -G docker ec2-user;
 curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose;
 chmod +x /usr/local/bin/docker-compose;
 iptables -t nat -A OUTPUT -o lo -p tcp --dport 80 -j REDIRECT --to-port 8000;
-git clone -b migrate-cloud https://github.com/jere1862/GelScraper.git;
+git clone https://github.com/jere1862/GelScraper.git;
 cd GelScraper/build/;
-echo "GEL_USERNAME=royj1933" >> .env;
+echo "GEL_USERNAME=" >> .env;
 echo "GEL_PASSWORD=" >> .env;
 echo "MESSENGER_KEY=" >> .env;
 echo "PAGE_URL=http://www.gel.usherbrooke.ca/s5info/a17/doc/evaluations/notesEtu.php" >> .env;
 /usr/local/bin/docker-compose build;
 /usr/local/bin/docker-compose up;
-
-// FIGURE OUT WHERE SCRIPT FAILS, ADD ECHOS
